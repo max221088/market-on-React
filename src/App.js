4ffc53af463d7e236b181be3c4b6618580d1d490
max221088 <117590988+max221088@users.noreply.github.com>
2023-09-07 22:46:59 +0300

    add header footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>
-      Shop
+    <div className='wrapper'>
+      <Header/>
+			<Footer/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+	return (
+		<header>
+			<div>
+				<span className='logo'>Hause Staff</span> 
+				<ul className='nav'>
+					<li>About</li>
+					<li>Contact</li>
+					<li>Cabinet</li>
+				</ul>
+			</div>
+			<div className='presentation'></div>
+		</header>
+	)
+}
